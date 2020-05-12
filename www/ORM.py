e4b3f1d7876901c2d6887168883be9be782ebc2e
= <jianghaoran1118@gmail.com>
2020-05-11 20:09:06 -0400

    finish the ORM structure

--- www/ORM.py
+++ www/ORM.py
@@ -1,4 +1,10 @@
 #day2
+import asyncio, logging,aiomysql
+
+def log(sql,args=()):
+	logging.info('SQL: %s' %sql)
+
+
 @asyncio.coroutine
 def creat_pool(loop, **kv):
 	logging.info("create database connection pool...")
@@ -40,6 +46,191 @@ def execute(sql, args):
 			yield from cur.execute(sql.replace('?', '%s'),args)
 			affected = cur.rowcount
 			yield from cur.close()
-		except: BaseException as e:
+		except BaseException as e:
 			raise
 		return affected
+
+
+
+class Field(object):
+
+	def __init__(self,name,column_type,primary_key,default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+
+
+	def __str__(self):
+		return '<%s,%s,%s>' %(self.__class__.__name__, self.column_type,cself.name)
+
+class StringField(Field):
+	def __init__(self, name = None, primary_key=False, default= None, ddl = 'varchar(100)'):
+		super().__init__(name,'boolean',primary_key,default)
+
+class BooleanField(Field):
+
+	def __init__(self, name=None, default = False):
+		super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+	def __init__(self, name = None, primary_key=False,default=0):
+		super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+	def __init__(self, name=None, primary_key=False, default=0):
+		super().__init__(name,'real',primary_key,default)
+
+class TextField(Field):
+	def __init__(self,name=None,default=None):
+		super().__init__(name, 'text',False,default)
+
+# build ModelMetaclass
+def create_args_string(num):
+	L = []
+	for n in range(num):
+		L.append('?')
+	return ', '.join(L)
+
+class ModelMetaclass(type):
+
+	def __new__(cls, name, bases, attrs):
+		if name == 'Model':
+			return type.__new__(cls, name, bases, attrs)
+
+	#get table name
+		tableName = attrs.get('__table__',None) or name
+		logging.info('found model: %s(table: %s)' %(name,tableName))
+	#get all fields and main keys
+		mappings = dict()
+		fields = []
+		primaryKey = None
+		for k, v in attrs.items():
+			if isinstance(v,Field):
+				logging.info(' found mapping: %s ==> %s' %(k, v))
+				mappings[k] = v 
+				if v.primary_key:
+					#find main key
+					if primaryKey:
+						raise RuntimeError('Duplicate primary key for field: %s' %k)
+					primaryKey = k 
+				else:
+					fields.append(k)
+		if not primaryKey:
+			raise RuntimeError('Primary key not found.')
+		for k in mappings.keys():
+			attrs.pop(k)
+
+		escaped_fields = list(map(lambda f: '`%s`' %f, fields))
+		attrs['__mappings__'] = mappings
+		attrs['__table__'] = tableName
+		attrs['__primary_key__'] = primaryKey#主键属性名
+		attrs['__fields__'] = fields #除主键外的属性名
+
+		#构造默认的select，insert,update 和delete语句
+		attrs['__select__'] = 'select `%s`, %s from `%s`' %(primaryKey,', '.join(escaped_fields),tableName)
+		attrs['__insert__'] = 'insert into `%s` (%s,`%s`) values (%s)' %(tableName, ', '.join(escaped_fields),primaryKey,create_args_string(len(escaped_fields)+1))
+		attrs['__update__'] = 'undate `%s` set %s where `%s`=?' % (tableName,', '.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f),fields)),primaryKey)
+		attrs['__delete__'] = 'delete from `%s` where `%s`=?' %(tableName,primaryKey)
+		return type.__new__(cls, name, bases, attrs)
+
+
+#ORM structure
+class Model(dict, metaclass=ModelMetaclass):
+
+	def __init__(self,**kw):
+		super(Model,self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attributes '%s'" %key)
+
+	def __setattr__(self,key,value):
+		self[key] = value
+	def getValue(self,key):
+		return getattr(self,key,None)
+
+
+	def getValueOrDefault(self,key):
+		value = getattr(self,key,None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s: %s' %(key, str(value)))
+				setattr(self,key,value)
+		return Value
+
+	@classmethod
+	async def findAll(cls, where=None, args=None, **kw):
+		##find objects by where clause
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = kw.get('orderBy', None)
+		if orderBy:
+			sql.append('order by')
+			sql.append(orderBy)
+		limit = kw.get('limit',None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit,int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit,tuple) and len(limit) == 2:
+				sql.append('?, ?')
+				args.append(limit)
+			else:
+				raise ValueError('Invalid limit value: %s' %str(limit))
+		rs = await select(''.join(sql), args)
+		return [cls(**r) for r in rs]
+
+	@classmethod
+	async def findNumber(cls, selectField,where = None, args=None):
+		#find number by select and where
+		sql = ['select %s _num_ from `%s`' %(selectField,cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = await select (''.join(sql),args,1)
+		if len(rs) == 0:
+			return None
+		return rs[0]['_num_']
+
+	@classmethod
+	async def find(cls,pk):
+		#find object by primary key
+		rs = await select('%s where `%s`=?' %(cls.__select__,cls.__primary_key__),[pk],1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+
+	async def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		args.append(self.getValueOrDefault(self.__primary_key__))
+		rows = await execute(self.__insert__, args)
+		if rows != 1:
+			logging.warning('failed to insert record: affected rows: %s' %rows)
+
+	async def update(self):
+		args = list(map(self.getValue, self.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		rows = await execute(self.__update__,args)
+		if rows != 1:
+			logging.warning('failed to update by primary key: affected rows: %s' %rows)
+
+	async def remove(self):
+		args = [self.getValue(self.__primary_key__)]
+		rows = await execute(self.__delete__, args)
+		if rows != 1:
+			logging.warning('failed to remove by primary key: affected rows: %s' %rows)
+
+	
+	
+
+
